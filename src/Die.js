49d3bb061ae69bafce3f9b139262b15095f1e76e
TMats <takomats@hotmail.com>
2021-11-29 14:37:40 -0500

    got the Die displaying dynamically from number 1 - 6

--- src/Die.js
+++ src/Die.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import './Die.css'
+
+let faDice = 'fas fa-dice-'
 
 class Die extends Component {
     constructor(props) {
         super(props);
+        this.state = { num: 'one' };
+        this.randomNum();
+    }
+    randomNum() {
+        let numArr = ['One', 'Two', 'Three', 'Four', 'Five', 'Six'];
+        let randNum = numArr[Math.floor(Math.random() * numArr.length)];
+        this.setState({ num: randNum });
     }
     render() {
+        let iconSrc = `${faDice}${this.state.num}`
         return(
             <div>
                 <h1>Hello from Die.js</h1>
+                <span className="Die">
+                    <i className={iconSrc}></i>
+                </span>
             </div>
         )
     }
